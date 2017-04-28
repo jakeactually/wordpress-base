cf5dc533b58deaac77cc00b04764fb6b94f722d6
JakeActually <jakeactually@outlook.com>
2017-04-28 16:25:57 -0500

    Stable

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
+    <div class="s clear"></div>
+
     <footer id="footer">
         <div class="wrap">
             <img src="<?= get_template_directory_uri() ?>/logo.png" alt="logo" height="50"><br>
-            <div class="s" style="height: 30px;"></div>
             <div>
                 Footer
             </div>
--- functions.php
+++ functions.php
@@ -5,11 +5,11 @@
     register_nav_menus(['main' => "Principal"]);
     
     function jc_enqueue_scripts() {
-        wp_enqueue_style('jc_sli', "https://unpkg.com/simple-line-icons@2.4.1/css/simple-line-icons.css");
-        wp_enqueue_style('jc_normalize', "https://unpkg.com/normalize.css@6.0.0");
-        wp_enqueue_style('jc_frontend', "https://unpkg.com/jakeactuallynext-frontend@1.0.12/frontend.css");
+        wp_enqueue_style('jc_sli', "https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css");
+        wp_enqueue_style('jc_normalize', "https://cdnjs.cloudflare.com/ajax/libs/normalize/6.0.0/normalize.min.css");
+        wp_enqueue_style('jc_frontend', get_template_directory_uri() . "/frontend.css");
         wp_enqueue_style('jc_style', get_stylesheet_uri());
-        wp_enqueue_script('jc_jquery', "https://unpkg.com/jquery@3.2.1/dist/jquery.min.js");
+        wp_enqueue_script('jc_jquery', "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js");
         wp_enqueue_script('jc_script', get_template_directory_uri() . "/script.js");
     }
     add_action('wp_enqueue_scripts', 'jc_enqueue_scripts');
