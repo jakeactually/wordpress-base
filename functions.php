9c97290895f8b2107a84f2eab7ee0299740cc232
JakeActually <jakeactually@outlook.com>
2017-09-23 18:51:42 -0500

    renamed styles

--- functions.php
+++ functions.php
@@ -9,10 +9,10 @@
         wp_enqueue_style('roboto', "https://fonts.googleapis.com/css?family=Roboto:400,700");
         wp_enqueue_style('normalize', "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css");
         wp_enqueue_style('frontend', get_template_directory_uri() . "/frontend.css");
-        wp_enqueue_style('style', get_stylesheet_uri());
+        wp_enqueue_style('wordpressBase', get_stylesheet_uri());
         wp_enqueue_script('hammer', "https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js");
-        wp_enqueue_script('jquery2', "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js");
-        wp_enqueue_script('script', get_template_directory_uri() . "/script.js");
+        wp_enqueue_script('$', "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js");
+        wp_enqueue_script('wordpressBase', get_template_directory_uri() . "/script.js");
     });
 
 ?>
